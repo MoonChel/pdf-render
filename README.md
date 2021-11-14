c4b09f6729e9b00c68ccf69ee6c798c97f2b9541
MoonChel <vlado.chernenko@gmail.com>
2021-11-14 14:48:07 +0100

    settings: refactor env vars, in docker or localhost

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 - alembic, gino, sqlalchemy
 - settings split prod / local / dev
+- rabbit vs zmq
+- tests
+
+# How to
+
+- `make run` - run app, available on http://127.0.0.1:8000/docs
--- api/settings.py
+++ api/settings.py
@@ -6,7 +6,9 @@ load_dotenv()
 
 _current_dir = os.path.dirname(os.path.realpath(__file__))
 
-SOCKET_URL = os.environ["CLIENT_SOCKET_URL"]
+SOCKET_HOST = os.environ["SOCKET_HOST"]
+SOCKET_POST = os.environ["SOCKET_POST"]
+SOCKET_URL = f"tcp://{SOCKET_HOST}:{SOCKET_POST}"
 
 FILE_FOLDER = os.path.join(_current_dir, "../files")
 IMAGE_FOLDER = os.path.join(_current_dir, "../images")
